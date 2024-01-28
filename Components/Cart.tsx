c310861ffe7c7dcda8ca154e8d6745f2269100c7
Zahrahyke <zahra@hyke.no>
2024-01-28 17:57:30 +0100

    update commit

--- Components/Cart.tsx
+++ Components/Cart.tsx
@@ -1,12 +1,32 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
-
+import Button from '../Components/Button'
 const Cart = () => {
   return (
-    <Box sx={{position:'absolute',height:'100vh',backgroundColor:'white'}}>
-    <Box sx={{position:'absolute',right:'500',minheight:'20vh',width:'30vw',backgroundcolor:'white'}}>
-      Cart
+    <Box sx={{position:'absolute',
+    height:'30vh',
+    width:'30vw',
+    left:800,
+    top:'2rem',
+    backgroundColor:'white'}}>
+        <Box sx={{
+        position:'absolute',
+        right:400,
+        minHeight:'20vh',
+        backgroundcolor:'white',
+        color:'1.5rem',
+        textTransform:'uppercase',
+        flexDirection:'column',
+    
+    }}>
+  <Box sx={{display:'flex',color:'black',justifyContent:'space-between'}}>
+    <Typography>Cart</Typography>
+    <Typography>Remove All</Typography>
+
     </Box>
+    <Button variant='contained' color='#f58321'>product</Button>
+  </Box>
+    
     </Box>
   )
 }
